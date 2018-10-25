59e91e66856ed7d812925e5d59845f23aaa857fa
Daniel Torres <daniel.torres@owasp.org>
2018-10-25 07:15:12 -0400

    update

--- instalar.sh
+++ instalar.sh
@@ -45,6 +45,9 @@ chmod a+x /usr/bin/XBruteForcer.pl
 cp passTelnet.pl /usr/bin
 cp crack-ntlm.sh /usr/bin
 
+cp generate-password.pl /usr/bin
+chmod a+x /usr/bin/generate-password.pl
+
 cp webCrack.sh /usr/bin
 chmod a+x /usr/bin/webCrack.sh
 

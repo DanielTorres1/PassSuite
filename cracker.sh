0536c7302bf8573fe2d906bbc76a946173d4f2ca
Daniel <daniel.torres@owasp.org>
2021-10-22 08:55:14 -0400

    update

--- cracker.sh
+++ cracker.sh
@@ -194,7 +194,7 @@ then
 				if [ -f ".vulnerabilidades2/"$ip"_"$port"_wpUsers.txt" ]; then
 					#https://181.115.188.36:443/				
 					for user in $(cat .vulnerabilidades2/"$ip"_"$port"_wpUsers.txt | wpscan-parser.py | awk '{print $2}'); do
-						echo -e "\t\t[+] Probando con usuario $user"
+						echo -e "\t\t[+] Probando con usuario ($user)"
 						#$ip = dominio
 						if [[ ${ip} == *"bo"* || ${ip} == *"com"*  || ${ip} == *"net"* || ${ip} != *"org"* || ${ip} != *"net"* ]];then 
 							real_ip=`host $ip | head -1 | cut -d " " -f4` 

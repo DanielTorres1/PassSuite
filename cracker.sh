13c01af3434f062ebd557f7969f7230b76764a3c
Daniel <daniel.torres@owasp.org>
2024-06-24 21:47:44 -0600

    update

--- cracker.sh
+++ cracker.sh
@@ -8,7 +8,6 @@ OKRED='\033[91m'
 OKGREEN='\033[92m'
 OKORANGE='\033[93m'
 RESET='\e[0m'
-LC_TIME=C
 
 # RDP crak
 
@@ -58,6 +57,10 @@ while (( "$#" )); do
       DICTIONARY=$2
       shift 2
       ;;
+	--country)
+      COUNTRY=$2
+      shift 2
+      ;;
 	--extratest)
       EXTRATEST=$2
       shift 2
@@ -74,9 +77,8 @@ tomcat_passwords_combo="/usr/share/lanscanner/tomcat-passwds.txt"
 FILE_SUBDOMAINS="importarMaltego/subdominios-scan.csv"
 MAX_SCRIPT_INSTANCES=30
 MIN_RAM=900
-COUNTRY='bolivia'
 
-echo "LENGUAJE $LENGUAJE MODE $MODE ENTIDAD(k) $ENTIDAD DICTIONARY $DICTIONARY EXTRATEST $EXTRATEST VERBOSE:$VERBOSE SPEED $SPEED"
+echo "LENGUAJE $LENGUAJE MODE $MODE ENTIDAD(k) $ENTIDAD DICTIONARY $DICTIONARY EXTRATEST $EXTRATEST VERBOSE:$VERBOSE SPEED $SPEED COUNTRY $COUNTRY"
 if [[ -z $LENGUAJE ]];then 
 
 cat << "EOF"
